94bb7be38b45cf8ee182ab4a0bb5e3b6a68706e0
akouryy <akouryy7@gmail.com>
2018-05-21 20:25:15 +0900

    Add evalSteps

--- lambda-without-type/js/lam.js
+++ lambda-without-type/js/lam.js
@@ -40,9 +40,48 @@ export function uniqueVar(tree, { viLast = new Map(), viStack = new Map() } = {}
   }
 }
 
+function assign(tree, from, to) {
+  if(tree instanceof VarTree) {
+    if(tree.name === from) {
+      return to;
+    } else {
+      return tree;
+    }
+  } else if(tree instanceof AppTree) {
+    return new AppTree(tree.info, assign(tree.fn, from, to), assign(tree.arg, from, to));
+  } else {
+    if(tree.arg === from) {
+      // 内部に同名の変数がある
+      return tree;
+    }
+    return new AbsTree(tree.info, tree.arg, assign(tree.body, from, to));
+  }
+}
+
+function evalStep(tree) {
+  if(tree instanceof AppTree) {
+    if(tree.fn instanceof AbsTree) {
+      return { newTree: assign(tree.fn.body, tree.fn.arg, tree.arg), changed: true };
+    } else {
+      const { newTree: newFn, changed: changedFn } = evalStep(tree.fn);
+      if(changedFn) {
+        return { newTree: new AppTree(tree.info, newFn, tree.arg), changed: true };
+      }
+      const { newTree: newArg, changed: changedArg } = evalStep(tree.arg);
+      if(changedArg) {
+        return { newTree: new AppTree(tree.info, tree.fn, newArg), changed: true };
+      }
+    }
+  }
+  return { newTree: tree, changed: false };
+}
+
 export function evalSteps(tree) {
-  const steps = [];
-  steps.push(new AppTree({}, new AbsTree({}, 'a$1', new VarTree({}, 'a$1')), new VarTree({}, 'a$2')));
-  steps.push(new VarTree({}, 'a$2'));
+  const steps = [tree];
+  while(true) {
+    const { newTree, changed } = evalStep(steps[steps.length - 1]);
+    if(!changed) break;
+    steps.push(newTree);
+  }
   return steps;
 }
